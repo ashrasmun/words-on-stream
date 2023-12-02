1b4b94baf9d0d5e711504c15157c7f0d61cf2b33
ashrasmun <ashrasmun@gmail.com>
2023-12-02 20:22:42 +0100

    Setup initial project stuff, along with virtual environment

---
+++ venv/venv_setup.bat
@@ -0,0 +1,36 @@
+@echo off
+rem Do not setlocal because it prevents the virtual environment from being
+rem activated
+
+rem Minimal version of python required to create the virtual environment and
+rem invoke the script.
+set min_py_version=3.12
+
+rem Name of the virtual environment. Should be transparent for the user.
+set venv_name=env
+
+:: -------------------------------- details -------------------------------- ::
+
+where python 2>nul 1>nul
+
+if errorlevel 1 (
+    echo You need to install Python %min_py_version% before setting up the
+    echo virtual environment.
+    exit /b 1
+)
+
+python -V > temp.txt
+set /p version_string=<temp.txt
+del temp.txt
+
+if "%version_string:~0,8%" NEQ "Python %min_py_version:~0,1%" (
+    echo You need to install Python %min_py_version% before setting up the
+    echo virtual environment.
+    exit /b 1
+)
+
+pushd %~dp0
+    py -%min_py_version% venv_setup.py
+    call %venv_name%\Scripts\activate
+    pip install ..[dev]
+popd
---
+++ venv/venv_setup.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+
+def run_py(command: str) -> subprocess.CompletedProcess:
+    py_ver = os.environ["min_py_version"]
+    cmd = f"py -{py_ver} {command}"
+    print(cmd)
+    return subprocess.run(
+        cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+def main():
+    print("Updating pip...")
+    run_py("-m pip install --upgrade pip")
+    print("Checking for virtualenv module...")
+    p = run_py("-m virtualenv")
+
+    if p.returncode != 0:
+        print("Installing virtualenv module...")
+        run_py("-m pip install virtualenv")
+
+    venv_name = os.environ["venv_name"]
+    venv_dir = os.path.join(__file__, "..", venv_name)
+
+    if not os.path.isdir(venv_dir):
+        print("Creating virtual environment...")
+        run_py(f"-m virtualenv {venv_dir}")
+
+
+if __name__=="__main__":
+    main()
